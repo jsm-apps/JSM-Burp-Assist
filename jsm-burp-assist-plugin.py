35b7ce4842cfce5c8add88170a502f764d69c866
jsm-apps <jobs.mminchinton@gmail.com>
2026-07-28 03:58:30 +0100

    fix

--- jsm-burp-assist-plugin.py
+++ jsm-burp-assist-plugin.py
@@ -94,7 +94,7 @@ class BurpExtender(IBurpExtender, IContextMenuFactory, ITab):
             self._print(
                 "Tech detection for {}".format(url)
             )
-            self._print(str(message))
+            self._print(str(response))
 
             task_id = self._results_manager.create_task(url)
 
