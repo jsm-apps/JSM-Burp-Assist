0c108723c5adb5e4df74ad864a0fa6e879aacc8b
jsm-apps <jobs.mminchinton@gmail.com>
2026-07-30 08:40:27 +0100

    updated debugtab

--- burp_plugin_libs/tabs/debugtab.py
+++ burp_plugin_libs/tabs/debugtab.py
@@ -1,4 +1,25 @@
-from javax.swing import JPanel, JLabel
+from java.awt.event import ActionListener
+from javax.swing import (
+    JPanel,
+    JLabel,
+    JButton,
+    JFrame
+)
+
+class DiscoveryAction(ActionListener):
+    def actionPerformed(self, event):
+        frame = JFrame("Discovery")
+        frame.setSize(400, 300)
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(None)
+
+        label = JLabel("Discovery")
+        label.setFont(Font("SansSerif", Font.BOLD, 16))
+        label.setBounds(20, 20, 200, 30)
+        frame.add(label)
+
+        frame.setLocationRelativeTo(None)  # Centre on screen
+        frame.setVisible(True)
 
 class DebugTab(object):
     def __init__(self, error_callback=None):
@@ -12,5 +33,10 @@ class DebugTab(object):
         label = JLabel("Debug Panel")
         label.setBounds(10, 10, 200, 25)
         self._panel.add(label)
+
+        discovery_button = JButton("Discovery")
+        discovery_button.setBounds(10, 50, 120, 30)
+        discovery_button.addActionListener(DiscoveryAction())
+        self._panel.add(discovery_button)
 
         return self._panel
