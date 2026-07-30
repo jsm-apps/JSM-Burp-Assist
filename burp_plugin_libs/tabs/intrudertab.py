ddb88cb2b18e9bda7224eb1fd04327d04a9e6b8a
jsm-apps <jobs.mminchinton@gmail.com>
2026-07-30 09:54:03 +0100

    added placeholder support

--- burp_plugin_libs/tabs/intrudertab.py
+++ burp_plugin_libs/tabs/intrudertab.py
@@ -7,8 +7,38 @@ from javax.swing import (
     JScrollPane,
     JTextArea
 )
+import re
 
 
+class AddMarkerAction(ActionListener):
+    def __init__(self, text_area):
+        self._text_area = text_area
+
+    def actionPerformed(self, event):
+        start = self._text_area.getSelectionStart()
+        end = self._text_area.getSelectionEnd()
+
+        # Nothing selected
+        if start == end:
+            return
+
+        selected = self._text_area.getSelectedText()
+        replacement = u"\u00A7%s\u00A7" % selected
+
+        self._text_area.replaceRange(replacement, start, end)
+        self._text_area.select(start, start + len(replacement))
+
+class ClearMarkerAction(ActionListener):
+    def __init__(self, text_area):
+        self._text_area = text_area
+
+    def actionPerformed(self, event):
+        text = self._text_area.getText()
+
+        # Replace §text§ with text
+        text = re.sub(u"\u00A7(.*?)\u00A7", r"\1", text)
+
+        self._text_area.setText(text)
 
 class IntruderTab(object):
     def __init__(self, error_callback=None):
@@ -18,6 +48,7 @@ class IntruderTab(object):
     def build_ui(self):
         self._panel = JPanel()
         self._panel.setLayout(None)
+        self._details_area = JTextArea()
 
         label = JLabel("Intruder Panel")
         label.setBounds(10, 10, 200, 25)
@@ -25,13 +56,14 @@ class IntruderTab(object):
 
         add_button = JButton("Add $")
         add_button.setBounds(10, 50, 120, 30)
+        add_button.addActionListener(AddMarkerAction(self._details_area))
         self._panel.add(add_button)
 
         clear_button = JButton("Clear $")
         clear_button.setBounds(180, 50, 120, 30)
+        clear_button.addActionListener(ClearMarkerAction(self._details_area))
         self._panel.add(clear_button)
 
-        self._details_area = JTextArea()
         self._details_area.setEditable(True)
         self._details_area.setLineWrap(False)
         details_scroll = JScrollPane(self._details_area)
