9418b017b77d5611e29666453720e384dc7bbb4c
jsm-apps <jobs.mminchinton@gmail.com>
2026-08-02 09:59:08 +0100

    decrease penalty

--- ai_libs/wordlistgenerator.py
+++ ai_libs/wordlistgenerator.py
@@ -44,7 +44,7 @@ class WordlistGenerator():
                 format=WordList.model_json_schema(),
                 think=False,
                 options={
-                    "repeat_penalty": 1.3
+                    "repeat_penalty": 1.2
                 }
             )
 
